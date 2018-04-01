d308a3cfbf54cee4e68114fe0ecf5f5ac94ef235
Eloy Eduardo Santillan Caballero <santillaneloy@hotmail.com>
2018-04-01 17:59:28 -0500

    Prueba 01

---
+++ Prossor/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prossor
+{
+    class GuerreroZ
+    {
+        public string Nombre { get; set; }
+        public string Raza { get; set; }
+        public char Sexo { get; set; }
+        public int Ki { get; set; }
+        public int Universo { get; set; }
+        public bool EstaVivo { get; set; }
+        public string Cargo { get; set; }
+        public Planeta Planeta { get; set; }
+    }
+
+    public class Planeta
+    {
+        public string Nombre { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dragon Ball");
+            GuerreroZ guerrero = new GuerreroZ();
+            guerrero.Nombre = "Goku";
+            ShorGuerrero(guerrero);
+            Console.ReadKey();
+        }
+
+
+        static void ShorGuerrero(GuerreroZ guerrero){
+            Console.WriteLine("===================");
+            Console.WriteLine("Guerrero: " + guerrero.Nombre);
+            Console.WriteLine("===================");
+        }
+    }
+}
